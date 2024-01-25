02959125f218fc5a8c13feb2058e9c94540f7b5e
jenifer miranda <jenifermiranda@MBPdegabriel2.Home>
2024-01-25 21:17:21 +0000

    feat: cria a interface Importer, cria a classe JsonImporter e a classe CsvImporter

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,17 +1,38 @@
-from typing import Dict, Type
+import json
+import csv
+from typing import Dict, List, Type
+from abc import ABC, abstractmethod
+
+from inventory_report.product import Product
+
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        pass
 
 
-class Importer:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> List[Product]:
+        with open(self.path, "r") as file:
+            data = json.load(file)
 
+        products = []
+        for product in data:
+            products.append(Product(**product))
+        return products
 
-
-class JsonImporter:
-    pass
-
-
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> List[Product]:
+        with open(self.path, "r") as file:
+            csv_reader = csv.DictReader(file)
+            products = []
+            for product in csv_reader:
+                products.append(Product(**product))
+            return products
+        
 
 
 # Não altere a variável abaixo
