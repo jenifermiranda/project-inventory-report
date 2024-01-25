f872de086d242a98fdbbb43446e55580b272c931
jenifer miranda <jenifermiranda@MBPdegabriel2.Home>
2024-01-25 20:24:19 +0000

    test: testa se o str do objeto Product retorna a frase correta

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,20 @@
+from inventory_report.product import Product
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        id="123",
+        product_name="Product-name",
+        company_name="Company-name",
+        manufacturing_date="2024-01-01",
+        expiration_date="2024-01-25",
+        serial_number="123456",
+        storage_instructions="Keep in a dry and cool place",
+    )
+
+    assert (
+        str(product)
+        == "The product 123 - Product-name with serial number 123456 "
+        "manufactured on 2024-01-01 by the company Company-name "
+        "valid until 2024-01-25 must be stored according to the "
+        "following instructions: Keep in a dry and cool place."
+    )
