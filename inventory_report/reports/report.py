db178dad0de4f97dbd8eab43e54aac1285d8d192
jenifer miranda <jenifermiranda@MBPdegabriel2.Home>
2024-01-26 20:01:36 +0000

    fix: garantindo que a classe Report seja um protocolo

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,7 +1,7 @@
 from inventory_report.inventory import Inventory
+from typing import Protocol
 
-
-class Report:
+class Report(Protocol):
     def add_inventory(self, inventory: Inventory) -> None:
         raise NotImplementedError
 
