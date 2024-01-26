b5e6699e4f77cbf80b3bb4c3cb1ce81c7b5ed39e
jenifer miranda <jenifermiranda@MBPdegabriel2.Home>
2024-01-26 19:10:05 +0000

    feat: cria a classe Inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from typing import List, Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None) -> None:
+        self.__data = data or []
+
+    def add_data(self, data: List[Product]) -> None:
+        self.__data.extend(data)
+
+    @property
+    def data(self) -> List[Product]:
+        return self.__data.copy()
