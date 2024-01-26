fd200a0954a35d1c4a011e3869ac1a3693c5b131
jenifer miranda <jenifermiranda@MBPdegabriel2.Home>
2024-01-26 19:13:54 +0000

    feat: cria o protocolo Report

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,9 @@
+from inventory_report.inventory import Inventory
+
+
 class Report:
-    pass
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.inventory = inventory
+
+    def generate(self) -> str:
+        raise NotImplementedError
