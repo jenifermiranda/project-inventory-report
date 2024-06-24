6940353765a98a3f1a6e7e17a584f1e91c6245ef
jenifer miranda <jenifermiranda@MBPdegabriel2.Home>
2024-06-24 18:01:59 +0100

    implementando report completo

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+class CompleteReport(SimpleReport):
+    def generate(self) -> str:
+        result = super().generate()
+        result += "\nStocked products by company:\n"
+
+        company_name = {}
+        for inventory in self.inventories:
+            for product in inventory.data:
+                if product.company_name in company_name:
+                    company_name[product.company_name] += 1
+                else:
+                    company_name[product.company_name] = 1
+        
+        for company, count in company_name.items():
+            result += f"- {company}: {count}\n"
+
+        return result
